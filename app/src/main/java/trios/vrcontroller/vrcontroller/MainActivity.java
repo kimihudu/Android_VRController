8d9be6d2fa7ca56c800d1505a3ab342119e51202
kimihudu <kimihudu@gmail.com>
2017-09-19 22:31:38 -0400

    test update

--- app/src/main/java/trios/vrcontroller/vrcontroller/MainActivity.java
+++ app/src/main/java/trios/vrcontroller/vrcontroller/MainActivity.java
@@ -2,6 +2,9 @@ package trios.vrcontroller.vrcontroller;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.widget.Toast;
+
+import static android.widget.Toast.LENGTH_SHORT;
 
 public class MainActivity extends Activity {
 
@@ -9,5 +12,6 @@ public class MainActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Toast.makeText(this.getBaseContext(),"test",LENGTH_SHORT).show();
     }
 }
