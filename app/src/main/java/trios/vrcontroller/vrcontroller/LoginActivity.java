8d05fe2ae3c49bfbd38c43b2404c4478973fb73f
kimihudu <kimihudu@gmail.com>
2017-09-20 10:15:56 -0400

    add drawable

--- app/src/main/java/trios/vrcontroller/vrcontroller/LoginActivity.java
+++ app/src/main/java/trios/vrcontroller/vrcontroller/LoginActivity.java
@@ -3,6 +3,7 @@ package trios.vrcontroller.vrcontroller;
 import android.animation.Animator;
 import android.animation.AnimatorListenerAdapter;
 import android.annotation.TargetApi;
+import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.support.annotation.NonNull;
 import android.support.design.widget.Snackbar;
@@ -28,6 +29,7 @@ import android.widget.AutoCompleteTextView;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -334,6 +336,10 @@ public class LoginActivity extends AppCompatActivity implements LoaderCallbacks<
 
             if (success) {
                 finish();
+                Toast.makeText(LoginActivity.this, "Congrats: Login Successfull", Toast.LENGTH_LONG).show();
+                Intent i = new Intent(LoginActivity.this, MapsActivity.class);
+//                i.putExtra("user", user.getUsrName());
+                startActivity(i);
             } else {
                 mPasswordView.setError(getString(R.string.error_incorrect_password));
                 mPasswordView.requestFocus();
