10cf4e8d6110778dfecfec0aa7dc5faac0eaa343
kimihudu <kimihudu@gmail.com>
2017-09-22 09:33:41 -0400

    - Edit layout Main - Add floating button btnAddLoc

--- app/src/main/java/trios/vrcontroller/vrcontroller/ListData.java
+++ app/src/main/java/trios/vrcontroller/vrcontroller/ListData.java
@@ -7,22 +7,25 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
 
+import static trios.vrcontroller.vrcontroller.R.id.btnAddLoc;
+
 public class ListData extends AppCompatActivity {
+
+    private FloatingActionButton btnAddLoc;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_list_data);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
+        btnAddLoc = (FloatingActionButton) findViewById(R.id.btnAddLoc);
+        btnAddLoc.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
                         .setAction("Action", null).show();
             }
         });
+
     }
 }
