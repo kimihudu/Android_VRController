dfc99c73203644069c2b041eafd37488cc8cb20f
kimihudu <kimihudu@gmail.com>
2017-09-22 09:36:27 -0400

    - Edit layout Main - Add floating button btnAddLoc

--- app/src/main/java/trios/vrcontroller/vrcontroller/ListData.java
+++ app/src/main/java/trios/vrcontroller/vrcontroller/ListData.java
@@ -11,21 +11,12 @@ import static trios.vrcontroller.vrcontroller.R.id.btnAddLoc;
 
 public class ListData extends AppCompatActivity {
 
-    private FloatingActionButton btnAddLoc;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_list_data);
-
-        btnAddLoc = (FloatingActionButton) findViewById(R.id.btnAddLoc);
-        btnAddLoc.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
 
     }
 }
--- app/src/main/java/trios/vrcontroller/vrcontroller/MainActivity.java
+++ app/src/main/java/trios/vrcontroller/vrcontroller/MainActivity.java
@@ -2,17 +2,29 @@ package trios.vrcontroller.vrcontroller;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.view.View;
 import android.widget.Toast;
 
 import static android.widget.Toast.LENGTH_SHORT;
 
 public class MainActivity extends Activity {
 
+    private FloatingActionButton btnAddLoc;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Toast.makeText(this.getBaseContext(),"test",LENGTH_SHORT).show();
-        Toast.makeText(this.getBaseContext(),"test",LENGTH_SHORT).show();
+
+        btnAddLoc = (FloatingActionButton) findViewById(R.id.btnAddLoc);
+        btnAddLoc.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });
+
     }
 }
