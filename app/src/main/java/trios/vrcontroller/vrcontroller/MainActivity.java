cd815098d0115e4dcf03dfef168c41bdc06ec190
kimihudu <kimihudu@gmail.com>
2017-09-22 09:50:50 -0400

    - change type class MainActivity --> AppCompatActivity

--- app/src/main/java/trios/vrcontroller/vrcontroller/MainActivity.java
+++ app/src/main/java/trios/vrcontroller/vrcontroller/MainActivity.java
@@ -4,20 +4,27 @@ import android.app.Activity;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
+import android.support.v4.app.FragmentActivity;
+import android.support.v7.app.AppCompatActivity;
+import android.view.LayoutInflater;
 import android.view.View;
+import android.support.v4.app.Fragment;
+import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.Toast;
 
 import static android.widget.Toast.LENGTH_SHORT;
 
-public class MainActivity extends Activity {
+public class MainActivity extends AppCompatActivity {
 
     private FloatingActionButton btnAddLoc;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        btnAddLoc = (FloatingActionButton) findViewById(R.id.btnAddLoc);
+        btnAddLoc =  (FloatingActionButton) findViewById(R.id.btnAddLoc);
         btnAddLoc.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
