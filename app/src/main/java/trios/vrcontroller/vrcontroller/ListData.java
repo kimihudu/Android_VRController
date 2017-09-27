317661ebe13ca0c5d40dc4adedec07427e2f06f6
jasjeet <jasjeetgill93@gmail.com>
2017-09-27 11:36:45 -0400

    update StreetViewImgs layout: --using two layout one for listview -- 2nd for text and image

--- app/src/main/java/trios/vrcontroller/vrcontroller/ListData.java
+++ app/src/main/java/trios/vrcontroller/vrcontroller/ListData.java
@@ -3,6 +3,7 @@ package trios.vrcontroller.vrcontroller;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 
@@ -16,6 +17,8 @@ public class ListData extends AppCompatActivity {
         setContentView(R.layout.content_list_data);
 
         Intent intent = getIntent();
+        TextView txloc = (TextView)findViewById(R.id.txtLoc);
+        ImageView imloc = (ImageView)findViewById(R.id.iconLoc);
 
 
     }
